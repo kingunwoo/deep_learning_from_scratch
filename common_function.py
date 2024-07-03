300da22898a7d74cfe24d8f2e077a5e2459556e3
kingunwoo <keonu1111@gmail.com>
2024-07-03 16:24:04 +0900

    3rd

--- common_function.py
+++ common_function.py
@@ -15,21 +15,23 @@ def im2col(input_data, filter_h, filter_w, stride = 1, pad = 0):
     #input_data is 4 dimension
     one_input_data = input_data[0][0]
     #zero th data,zero th chanel
-    OH = (one_input_data.shape[0] + (2*pad) - filter_h)/stride + 1
-    OW = (one_input_data.shape[1]+(2*pad) - filter_w)/stride + 1
+    OH = (one_input_data.shape[0] + (2*pad) - filter_h)//stride + 1
+    OW = (one_input_data.shape[1]+(2*pad) - filter_w)//stride + 1
     out_matrix_col_len = filter_h * filter_w
     out_matrix_row_len = OH * OW
-    out_matrix = np.array(out_matrix_row_len, out_matrix_col_len)
+    out_matrix = np.zeros(out_matrix_row_len, out_matrix_col_len)
     
     flatten = np.reshape(input_data,(-1,one_input_data.shape[0],one_input_data.shape[1]))
     #4dim -> 3dim
     
+    position_count = 0 
     for mat in flatten:
         h_pos = 0
-        w_pos = 0
-        
-        
-        
+        for h in OH:
+            w_pos = 0
+            for w in OW:
+                out_matrix[position_count]= mat[h_pos:h_pos+filter_h, w_pos:w_pos+filter_w].flatten()
+                position_count += 1
     
     return out_matrix
 
