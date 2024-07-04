460bc927953f52096a674531a7633e9f8a91848c
kingunwoo <keonu1111@gmail.com>
2024-07-04 16:46:04 +0900

    7/4 second

--- make_Layer.py
+++ make_Layer.py
@@ -16,36 +16,36 @@ class convolution:
         
     def forward(self,input_data):
         flatten_img = common_functions.im2col(input_data, self.filter_h, self.filter_w, self.stride, self.pad)
-        filter.reshape(-1, self.filter_n)
-        flatten_conv_img = np.dot(flatten_img,filter)
+        flatten_filter = self.filter.reshape(self.filter_n, -1).T
+        flatten_conv_img = np.dot(flatten_img, flatten_filter)
         output_img = common_functions.col2im(flatten_conv_img, input_data.shape, self.filter_h, self.filter_w, self.stride, self.pad)
         return output_img
         
     def backward(self):
         return 
 
-class fc:
-    def __init__(self,input_data,length_output_node,activation): 
-        self.output = np.ones(length_output_node)
-        self.input = input_data.reshape(-1,784)
-        self.activation = activation
-        if(activation == activation_functions.sigmoid or activation == activation_functions.tanh):
-        else:
-            gfg
-    def forward(self):
-        output = np.dot(input,weight) + bias
-        return output        
-    def backward(self):
-        return
-    
-class pooling:
-    def __init__(self, pool_h, pool_w,  mode='max', pad = 0, stride = 1):
+#lass fc:
+#   def __init__(self,input_data,length_output_node,activation): 
+#       self.output = np.ones(length_output_node)
+#       self.input = input_data.reshape(-1,784)
+#       self.activation = activation
+#       if(activation == activation_functions.sigmoid or activation == activation_functions.tanh):
+#       else:
+#           gfg
+#   def forward(self):
+#       output = np.dot(input,weight) + bias
+#       return output        
+#   def backward(self):
+#       return
+#   
+#lass pooling:
+#   def __init__(self, pool_h, pool_w,  mode='max', pad = 0, stride = 1):
     
         
 if __name__ == "__main__":
-    import common_functions  # common_functions ¸ðµâ °¡Á®¿À±â
+    import common_functions  # common_functions ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 
-    input_data = np.random.randn(1, 1, 28, 28)  # ¿¹½Ã ÀÔ·Â µ¥ÀÌÅÍ
+    input_data = np.random.randn(1, 1, 28, 28)  # ï¿½ï¿½ï¿½ï¿½ ï¿½Ô·ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
     conv_layer = convolution(filter_n=6, filter_h=5, filter_w=5, pad=2, stride=1, activation='sigmoid')
     output_data = conv_layer.forward(input_data)
     print(output_data.shape)
